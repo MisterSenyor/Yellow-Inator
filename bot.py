cfa92b04edb607d07b28211e2df9a2e246257305
Liam Mohr <liam.mohr.45@gmail.com>
2024-09-30 14:41:16 +0300

    started first alert

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
 import points
+import alert
 
 
 
@@ -12,6 +13,7 @@ def main():
     app = ApplicationBuilder().token("7899662823:AAHg34XX6f2HedB9ONi_XArgTCgE4hv6q5E").build()
     app.add_handler(CommandHandler("hello", hello))
     app.add_handler(CommandHandler("points", points.points_prompts[0]["prompt"]))
+    app.add_handler(CommandHandler("first_alert", points.alert_prompts[0]["prompt"]))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, points.handle_number_input))  # Handle the number input
     app.add_handler(CallbackQueryHandler(points.button_handler))
     app.run_polling()
